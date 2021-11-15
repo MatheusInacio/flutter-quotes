3486cf12c36415c10987a77a43d938d7afe13dab
Matheus <mmatheus.inacio@outlook.com>
2021-11-15 15:53:06 -0300

    deletando quotes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/quote.dart';
+import 'package:http/http.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
@@ -12,21 +13,13 @@ class QuoteList extends StatefulWidget {
   _QuoteListState createState() => _QuoteListState();
 }
 
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(text: 'Rá ie ié', author: 'Malandro'),
-    Quote(
-        text: 'Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!!',
-        author: 'Cumpadi'),
-    Quote(
-        text: 'E que abundância meu irmão viuu!! Assim você vai matar o papai.',
-        author: 'Cumpadi'),
-    Quote(
-        text: 'Só digo uma coisa, Domingo ela não vai! Danadaa!!',
-        author: 'Cumpadi'),
-  ];
+class QuoteCard extends StatelessWidget {
+  final Quote quote;
+  final VoidCallback delete;
+  QuoteCard({required this.quote, required this.delete});
 
-  Widget quoteTemplate(Quote quote) {
+  @override
+  Widget build(BuildContext context) {
     return Card(
       margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
       child: Padding(
@@ -45,22 +38,57 @@ class _QuoteListState extends State<QuoteList> {
               quote.author,
               style: TextStyle(fontSize: 18, color: Colors.grey[800]),
             ),
+            SizedBox(
+              height: 8,
+            ),
+            TextButton.icon(
+                onPressed: delete,
+                icon: Icon(Icons.delete, color: Colors.red[700]),
+                label: Text(
+                  'Excluir',
+                  style: TextStyle(color: Colors.red[700]),
+                ))
           ],
         ),
       ),
     );
   }
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: 'Rá ie ié', author: 'Malandro'),
+    Quote(
+        text: 'Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!!',
+        author: 'Cumpadi'),
+    Quote(
+        text: 'E que abundância meu irmão viuu!! Assim você vai matar o papai.',
+        author: 'Cumpadi'),
+    Quote(
+        text: 'Só digo uma coisa, Domingo ela não vai! Danadaa!!',
+        author: 'Cumpadi'),
+    Quote(text: 'É hora do show!!!', author: 'Bambam'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.grey[50],
         appBar: AppBar(
           title: Text('Quotes Incríveis'),
           centerTitle: true,
-          backgroundColor: Colors.teal[800],
+          backgroundColor: Colors.indigo[800],
         ),
         body: Column(
-            children: quotes.map((quote) => quoteTemplate(quote)).toList()));
+            children: quotes
+                .map((quote) => QuoteCard(
+                    quote: quote,
+                    delete: () {
+                      setState(() {
+                        quotes.remove(quote);
+                      });
+                    }))
+                .toList()));
   }
 }
